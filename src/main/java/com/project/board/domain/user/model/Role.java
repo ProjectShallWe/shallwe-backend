f7bb57c285224b5d3a3ed054a4e2c8e7138d08d5
cjsrhd94 <cjsrhd94@gmail.com>
2022-05-31 02:56:21 +0900

    feat: User Entity 구현

---
+++ src/main/java/com/project/board/domain/user/model/Role.java
@@ -0,0 +1,6 @@
+package com.project.board.domain.user.model;
+
+public enum Role {
+    ADMIN,
+    USER;
+}
---
+++ src/main/java/com/project/board/domain/user/model/User.java
@@ -0,0 +1,39 @@
+package com.project.board.domain.user.model;
+
+import com.project.board.global.model.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+
+@Entity
+@Table(name = "user_table")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class User extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    // 사용자 아이디
+    @Email
+    @Column(name = "user_email", unique = true)
+    private String Email;
+
+    // 사용자 비밀번호
+    @Column(name = "user_password")
+    private String password;
+
+    @Column(name = "user_nickname")
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_role")
+    private Role role;
+}
