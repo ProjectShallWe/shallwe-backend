7f58ea52a284a9cd8a3539c99cb8588a336b5210
cjsrhd94 <cjsrhd94@gmail.com>
2022-05-31 22:32:19 +0900

    refactor: 스프링 버전 다운에 따른 WebSecurityCOnfigurerAdapter 사용

--- src/main/java/com/project/board/global/security/SecurityConfig.java
+++ src/main/java/com/project/board/global/security/SecurityConfig.java
@@ -3,21 +3,23 @@ package com.project.board.global.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
+                .authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and()
                 .httpBasic(withDefaults());
-        return http.build();
     }
 
 }
