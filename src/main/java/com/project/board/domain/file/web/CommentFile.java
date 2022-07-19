23e17180178d0f5460d9aa34e7f364b2ed232ae3
cjsrhd94 <cjsrhd94@gmail.com>
2022-07-20 00:03:29 +0900

    refactor: file 관련 entity Column 이름 변경 및 제약 조건 추가
    1. 기존에는 @column(name = ) 옵션을 통해 컬럼 마다 (테이블명_컬럼명)과 같은 방식으로 컬럼 이름을 재설정해주었습니다.
    2. 그러나 이것이 가독성이 떨어져 불필요하다는 판단에 삭제해주었습니다.
    3. column의 필요에 따라 @NotNull 옵션을 달아주었습니다.

--- src/main/java/com/project/board/domain/file/web/CommentFile.java
+++ src/main/java/com/project/board/domain/file/web/CommentFile.java
@@ -1,7 +1,7 @@
-package com.project.board.domain.file;
+package com.project.board.domain.file.web;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.project.board.domain.post.web.Post;
+import com.project.board.domain.comment.web.Comment;
 import com.project.board.global.audit.BaseEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -10,42 +10,42 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "post_file_table")
+@Table(name = "comment_file_table")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-public class PostFile extends BaseEntity {
+public class CommentFile extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "post_file_id")
     private Long id;
 
     // 전송 받은 파일 이름
-    @Column(name = "post_file_upload_file_name")
+    @NotNull
     private String uploadFileName;
 
     // UUID가 적용된 서버 저장용 파일 이름
-    @Column(name = "post_file_store_file_name")
+    @NotNull
     private String storeFileName;
 
     // 파일 저장 경로
-    @Column(name = "post_file_file_url")
+    @NotNull
     private String fileUrl;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JsonBackReference("postfile-post")
-    @JoinColumn(name = "post_id")
-    private Post post;
+    @JsonBackReference("commentfile-comment")
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
 
     @Builder
-    public PostFile(Long id, String uploadFileName, String storeFileName, String fileUrl, Post post) {
+    public CommentFile(Long id, String uploadFileName, String storeFileName, String fileUrl, Comment comment) {
         this.id = id;
         this.uploadFileName = uploadFileName;
         this.storeFileName = storeFileName;
         this.fileUrl = fileUrl;
-        this.post = post;
+        this.comment = comment;
     }
 }
--- src/main/java/com/project/board/domain/file/web/PostFile.java
+++ src/main/java/com/project/board/domain/file/web/PostFile.java
@@ -1,7 +1,7 @@
-package com.project.board.domain.file;
+package com.project.board.domain.file.web;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.project.board.domain.comment.web.Comment;
+import com.project.board.domain.post.web.Post;
 import com.project.board.global.audit.BaseEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -10,42 +10,43 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "comment_file_table")
+@Table(name = "post_file_table")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-public class CommentFile extends BaseEntity {
+public class PostFile extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "comment_file_id")
+    @Column(name = "post_file_id")
     private Long id;
 
     // 전송 받은 파일 이름
-    @Column(name = "comment_file_upload_file_name")
+    @NotNull
     private String uploadFileName;
 
     // UUID가 적용된 서버 저장용 파일 이름
-    @Column(name = "comment_file_store_file_name")
+    @NotNull
     private String storeFileName;
 
     // 파일 저장 경로
-    @Column(name = "comment_file_file_url")
+    @NotNull
     private String fileUrl;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JsonBackReference("commentfile-comment")
-    @JoinColumn(name = "comment_id")
-    private Comment comment;
+    @JsonBackReference("postfile-post")
+    @JoinColumn(name = "post_id")
+    private Post post;
 
     @Builder
-    public CommentFile(Long id, String uploadFileName, String storeFileName, String fileUrl, Comment comment) {
+    public PostFile(Long id, String uploadFileName, String storeFileName, String fileUrl, Post post) {
         this.id = id;
         this.uploadFileName = uploadFileName;
         this.storeFileName = storeFileName;
         this.fileUrl = fileUrl;
-        this.comment = comment;
+        this.post = post;
     }
 }
