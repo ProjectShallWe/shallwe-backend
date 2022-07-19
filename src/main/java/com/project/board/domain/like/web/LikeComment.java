07f1636a845bcba4525dd6004949a928b0f860f2
cjsrhd94 <cjsrhd94@gmail.com>
2022-07-19 23:35:55 +0900

    refactor: like 관련 entity Column 이름 변경 및 제약 조건 추가
    1. 기존에는 @Column(name = ) 옵션을 통해 컬럼 마다 (테이블명_컬럼명)과 같은 방식으로 컬럼 이름을 재설정해주었습니다.
    2. 그러나 이것이 가독성이 떨어지져 불필요하다는 판단에 삭제해주었습니다.

--- src/main/java/com/project/board/domain/like/web/LikeComment.java
+++ src/main/java/com/project/board/domain/like/web/LikeComment.java
@@ -21,7 +21,6 @@ public class LikeComment extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "like_comment_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/com/project/board/domain/like/web/LikePost.java
+++ src/main/java/com/project/board/domain/like/web/LikePost.java
@@ -18,7 +18,6 @@ public class LikePost extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "like_post_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
