37d4acaf72cddeb7262214cce95cd0c06d5edde1
cjsrhd94 <cjsrhd94@gmail.com>
2022-07-19 23:22:49 +0900

    chore: file 관련 repository 삭제
    1. file과 관련된 기능들은 향후 프론트 개발과 함께 웹에디터를 사용하여 구현하려합니다. 이에 사용하지 않는 repository들을 삭제합니다.

--- src/main/java/com/project/board/infrastructure/file/CommentFileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.board.infrastructure.file;
-
-import com.project.board.domain.file.CommentFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentFileRepository extends JpaRepository<CommentFile, Long> {
-}
--- src/main/java/com/project/board/infrastructure/file/PostFileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.board.infrastructure.file;
-
-import com.project.board.domain.file.PostFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostFileRepository extends JpaRepository<PostFile, Long> {
-}
