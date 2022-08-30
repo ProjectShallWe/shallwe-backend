d36e8069f8bdb551c1acbec8d48705d3cb4daf63
cjsrhd94 <cjsrhd94@gmail.com>
2022-08-30 21:22:06 +0900

    feat: CORS 처리

--- src/main/java/com/project/board/global/security/SecurityConfig.java
+++ src/main/java/com/project/board/global/security/SecurityConfig.java
@@ -12,6 +12,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +28,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    };
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -42,7 +59,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .authorizeRequests()
                 .antMatchers("/login").permitAll()
-                .anyRequest().permitAll();
+                .antMatchers("/**").permitAll()
+                .anyRequest().permitAll()
+
+                .and()
+                .cors();
     }
 
 }
