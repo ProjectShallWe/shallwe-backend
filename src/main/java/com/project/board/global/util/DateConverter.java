98c9615926cd45bc145d5adaeebe9ddd6c7586bb
cjsrhd94 <cjsrhd94@gmail.com>
2022-10-31 08:15:27 +0900

    feat: convertLocalDateTimeToMMdd 메서드 로직 수정
    1. convertLocalDateTimeToMMdd 메서드가 오늘 일 때 HH:mm의 형태로 return하게 변경하였습니다.

--- src/main/java/com/project/board/global/util/DateConverter.java
+++ src/main/java/com/project/board/global/util/DateConverter.java
@@ -6,7 +6,20 @@ import java.time.format.DateTimeFormatter;
 public class DateConverter {
 
     public static String convertLocalDateTimeToMMdd(LocalDateTime localDateTime) {
-        return localDateTime.format(DateTimeFormatter.ofPattern("MM-dd"));
+        if (isToday(localDateTime)) {
+            return localDateTime.format(DateTimeFormatter.ofPattern("HH:mm"));
+        } else {
+            return localDateTime.format(DateTimeFormatter.ofPattern("MM-dd"));
+        }
+    }
+    private static Boolean isToday(LocalDateTime localDateTime) {
+        String now = convertLocalDateTimeFromYearToDate(LocalDateTime.now());
+        String date = convertLocalDateTimeFromYearToDate(localDateTime);
+        return now.equals(date);
+    }
+
+    private static String convertLocalDateTimeFromYearToDate(LocalDateTime localDateTime) {
+        return localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 
     public static String convertLocalDateTimeFromYearToMinute(LocalDateTime localDateTime) {
